f25b3b348a6582c506a1362c4594d0542cc4d37e
vikhtorowitch <alikasoft@gmail.com>
2015-10-05 00:15:07 +0300

    add points

---
+++ Assets/Scripts/gameplay/GlobalPrefs.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalPrefs
+{
+	public static GlobalPrefs Instance { get; private set; }
+
+	private int points;
+	private int health;
+
+	public void Awake()	{
+		Instance = this;
+		points = 0;
+	}
+
+	public void makeDamage(int _hits) {
+		health -= _hits;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void addPoints(int _points) {
+		points += _points;	
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+}
